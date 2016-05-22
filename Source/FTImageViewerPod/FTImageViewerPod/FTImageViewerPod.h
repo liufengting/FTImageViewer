fe169c7b9e24cb3ce5922b414679bd45c34644fb
liufengting <wo157121900@me.com>
2016-05-22 21:57:56 +0800

    1.3.1

---
+++ Source/FTImageViewerPod/FTImageViewerPod/FTImageViewerPod.h
@@ -0,0 +1,19 @@
+//
+//  FTImageViewerPod.h
+//  FTImageViewerPod
+//
+//  Created by liufengting on 16/5/22.
+//  Copyright © 2016年 liufengting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FTImageViewerPod.
+FOUNDATION_EXPORT double FTImageViewerPodVersionNumber;
+
+//! Project version string for FTImageViewerPod.
+FOUNDATION_EXPORT const unsigned char FTImageViewerPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FTImageViewerPod/PublicHeader.h>
+
+
